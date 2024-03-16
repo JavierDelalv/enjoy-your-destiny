d5a4708c674c64477607e4505ff0ea007554af09
JavierDelalv <javier.delalv@gmail.com>
2024-03-17 00:22:46 +0100

    prueba

---
+++ src/app/components/destinos/alojamientos/alojamientos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alojamientos',
+  standalone: true,
+  imports: [],
+  templateUrl: './alojamientos.component.html',
+  styleUrl: './alojamientos.component.scss'
+})
+export class AlojamientosComponent {
+
+}
---
+++ src/app/components/destinos/restauracion/restauracion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restauracion',
+  standalone: true,
+  imports: [],
+  templateUrl: './restauracion.component.html',
+  styleUrl: './restauracion.component.scss'
+})
+export class RestauracionComponent {
+
+}
